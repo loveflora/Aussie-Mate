829074adef337c2465640fa64da0218eafa55c08
saywhat <95282021+loveflora@users.noreply.github.com>
2025-07-10 19:25:32 +1000

    Initial commit

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -4,7 +4,7 @@ export default {
   "expo": {
     "name": "WHMate",
     "slug": "WHMate",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "orientation": "portrait",
     "icon": "./assets/images/icon.png",
     "scheme": "whmate",
